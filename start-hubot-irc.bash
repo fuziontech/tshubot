480c4b6726bb00ec0a55dba4f6b44748e2a32b4e
James Greenhill <fuziontech@gmail.com>
2014-09-11 19:52:18 -0700

    add HEROKU_URL env var

--- start-hubot-irc.bash
+++ start-hubot-irc.bash
@@ -4,4 +4,5 @@ HUBOT_IRC_SERVER=irc.freenode.net \
 HUBOT_IRC_ROOMS="#tunnelsnakes" \
 HUBOT_IRC_NICK="tsbot" \
 HUBOT_IRC_UNFLOOD="true" \
+HEROKU_URL=http://fathomless-bayou-8263.herokuapp.com \
 bin/hubot -a irc --name tsbot 
