39c329e75bcce94cdd8d2b69e74d66b67f52d028
Jams <James@disqus.com>
2016-01-26 12:24:26 -0800

    hubot meet slack

--- start-hubot-irc.bash
+++ start-hubot-irc.bash
@@ -5,4 +5,5 @@ HUBOT_IRC_ROOMS="#tunnelsnakes" \
 HUBOT_IRC_NICK="tsbot" \
 HUBOT_IRC_UNFLOOD="true" \
 HEROKU_URL=http://tsbot.herokuapp.com \
+HUBOT_SLACK_TOKEN=xoxb-12606741143-M2Vuizagpv2WWre9wLqhKO7t \
 bin/hubot -a irc --name tsbot
