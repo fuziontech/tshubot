ea1b3bafd1de9557ddd0c8cf446a483ceca56056
Jams <James@disqus.com>
2016-01-26 12:28:23 -0800

    enable slack adapter

--- start-hubot-irc.bash
+++ start-hubot-irc.bash
@@ -6,4 +6,4 @@ HUBOT_IRC_NICK="tsbot" \
 HUBOT_IRC_UNFLOOD="true" \
 HEROKU_URL=http://tsbot.herokuapp.com \
 HUBOT_SLACK_TOKEN=xoxb-12606741143-M2Vuizagpv2WWre9wLqhKO7t \
-bin/hubot -a irc --name tsbot
+bin/hubot -a slack --name tsbot
